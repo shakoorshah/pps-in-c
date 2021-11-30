25ed7d3742966d2c3e8b2069c4b81b1b9495c44b
Shakoor Shah <shakoorshah08@gmail.com>
2021-11-30 14:34:41 +0530

    Capitalized E and R

--- reverse of a no..cpp
+++ reverse of a no..cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int n, r, sum=0;
-	printf("enter the number");
+	printf("Enter the number");
 	scanf("%d", &n);
 	while(n>0)
 	{
@@ -11,5 +11,5 @@ int main()
 		sum= (sum*10)+r;
 		n=n/10;
 	}
-	printf("reverse of the given number is: %d", sum);
+	printf("Reverse of the given number is: %d", sum);
 }
